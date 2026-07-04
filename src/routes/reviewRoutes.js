9b9e12aaca1cd5a9a2c7bc00e532c7c7e5e89950
Mengkim21 <mengkim1221@gmail.com>
2026-07-04 17:19:52 +0700

    add review logic

---
+++ src/routes/reviewRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { 
+  createReview, 
+  deleteReview, 
+  getReviews, 
+  updateReview 
+} from '../controllers/reviewController.js';
+import { auth } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.get('/get', auth, getReviews);
+router.post('/add', auth, createReview);
+router.patch('/update', auth, updateReview);
+router.delete('/delete', auth, deleteReview);
+
+export default router;
